a278604e767c757631e1a1a028b8dea64a76478c
Muhammad Saqlain <2mesaqlain@gmail.com>
2023-08-14 09:59:53 -0700

    axios added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Autocomplete from "react-autocomplete";
+import axios from "axios";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+  const ApiURL = `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${value}`;
+
+  useEffect(() => {
+    try {
+      axios.get(ApiURL).then((res) => {
+        const parsedData = [];
+
+        for (let i = 0; i < res.data[1].length; i++) {
+          parsedData.push({ id: i, label: res.data[1][i] });
+        }
+        setList(parsedData);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [value]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Autocomplete
+        getItemValue={(item) => item.label}
+        items={list}
+        renderItem={(item, isHighlighted) => (
+          <div
+            style={{ background: isHighlighted ? "lightgray" : "white" }}
+            key={item.label}
+          >
+            {item.label}
+          </div>
+        )}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onSelect={(val) => setValue(val)}
+      />
     </div>
   );
 }
