655c1e5e3495116279b82a5410b5c549eb4f22ef
Muhammad Saqlain <2mesaqlain@gmail.com>
2023-08-21 08:15:46 -0700

    give new look to UI

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,13 +3,23 @@ import React from "react";
 import Container from "../../components/Container";
 import AutoComplete from "../../components/Autocomplete";
 
+import "./styles.scss";
+import logo from "./logo.png";
+
 const Home = () => {
   return (
-    <Container>
-      {({ list, onValueChange, value }) => (
-        <AutoComplete list={list} onValueChange={onValueChange} value={value} />
-      )}
-    </Container>
+    <div className="home-page-container">
+      <img src={logo} alt="logo" />
+      <Container>
+        {({ list, onValueChange, value }) => (
+          <AutoComplete
+            list={list}
+            onValueChange={onValueChange}
+            value={value}
+          />
+        )}
+      </Container>
+    </div>
   );
 };
 
