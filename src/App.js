ab03961862e660d38628294f6b13ef402edebd72
Muhammad Saqlain <2mesaqlain@gmail.com>
2023-08-21 00:10:05 -0700

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
 
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Container from "./components/Container";
-import AutoComplete from "./components/Autocomplete";
+import Home from "./pages/home";
+import Search from "./pages/search";
+import NotFound from "./pages/not-found";
 
 function App() {
   return (
-    <Container>
-      {({ list, onValueChange, value }) => (
-        <AutoComplete list={list} onValueChange={onValueChange} value={value} />
-      )}
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+
+    // <Container>
+    //   {({ list, onValueChange, value }) => (
+    //     <AutoComplete list={list} onValueChange={onValueChange} value={value} />
+    //   )}
+    // </Container>
   );
 }
 
